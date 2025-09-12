36f3393e24d5e696d2498357211f27bd54ddcd14
WesleyTaveira <wesleylucas.taveira@gmail.com>
2025-09-12 02:22:50 -0300

    feat(api-controller-carro): criando o get, post, put e delete

--- src/controller/carro.controller.ts
+++ src/controller/carro.controller.ts
@@ -1,19 +1,29 @@
 import { Router } from "express"
+import {v4 as uuidV4} from 'uuid'
+import { Carro } from "src/model/Carro"
 
 const router = Router();
+
+
+let carros: Carro[] = [];
 
 router.get("/carro", (req, res) => {
     return res.status(200).send("Olá mundo!")
 })
 
-/* router.get("/:id/visualizar", (req, res) => {
-    return res.status(200).send(req.params.id)
-}) */
-
+ router.get("/:id", (req, res) => {
+    const{id} = req.params;
+    const carro = carros.find(carro => carro.id === id)
+}) 
+                                                         
 router.post("/carro", (req, res) =>{
-    const {id, placa, ano, marca} = req.body
+    const carro = req.body                      
+    carro.id = uuidV4();
+    carros.push(carro)
+   
+  res.status(201).json(carro)
+});
 
-    return res.status(201).send()
-})
 
+                                         
 export default { router } 
--- src/model/Marca.ts
+++ src/model/Marca.ts
@@ -1,5 +1,5 @@
  import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
- import { Carro } from "./Carro"; 
+ import { Carro } from "./Carro"
 
 @Entity()
 export class Marca {
