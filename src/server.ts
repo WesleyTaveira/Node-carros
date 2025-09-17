3d9dcb0d3a098c35fba0d1ba07605945d28041ef
WesleyTaveira <wesleylucas.taveira@gmail.com>
2025-09-16 23:52:08 -0300

    ajustes banco

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,6 @@ import express from 'express';
 import router from './controller';
 import { AppDataSource } from "./data-source";
 
-/* const app = express();
-
-app.use(express.json());
-app.get("/", (req,res) => {
-    const valor = req.query.valor;
-    console.log("valor >>>",valor)
-    return res.status(200).json({
-        status: true,
-        value: valor
-    })
-})
-
-app.use(router);
-app.listen(3333, () => {
-    console.log('server 3333')
-});  */
 
 AppDataSource.initialize()
     .then(() => {
